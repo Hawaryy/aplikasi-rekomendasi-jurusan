c5ca88a2fe6bd27eb8ca290bb7ffe15bb4832903
Hawaryy <ikhsankhawari@gmail.com>
2025-12-20 05:32:08 +0700

    second commit

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,1 +1,12 @@
+import sys
+import os
+
+# Tambahkan path aplikasi
+sys.path.insert(0, os.path.dirname(__file__))
+
+# Import Flask app dari app.py
 from app import app as application
+
+# Untuk compatibility dengan berbagai WSGI server
+if __name__ == "__main__":
+    application.run()
